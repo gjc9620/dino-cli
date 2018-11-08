e815153fb51a72cedb0c910655d4ea67b5099452
gjc9620 <992758355@qq.com>
2018-11-08 21:08:45 +0800

    cli

--- template/dinoProject/client/modules/mobile/myModule/page/toDoList/toDoList.jsx
+++ template/dinoProject/client/modules/mobile/myModule/page/toDoList/toDoList.jsx
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
+// import { connect } from 'react-redux';
 import View from 'myModule/View';
 import commonFunction from 'myModule/commonFunction';
 
